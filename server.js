b7cf29a40e3cd9bb80f3ed7c935acf6ee685cd93
Joao Adherval <joaoadherval@gmail.com>
2021-08-25 19:27:42 -0300

    connecting to mongo database

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require("mongoose");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 
