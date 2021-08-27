84d1a1a1189d7e43aabcd10ef141e4159b8fe82c
Joao Adherval <joaoadherval@gmail.com>
2021-08-26 21:30:38 -0300

    creating /api/shorturl post endpoint and building the json response accordingly

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -12,7 +13,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const Schema = mongoose.Schema;
 
 const urlSchema = new Schema({
-  original_url: { type: String, required: true }
+  original_url: { type: String, required: true },
+  short_url: Number
 });
 
 const Url = mongoose.model('Url', urlSchema);
@@ -32,4 +34,36 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+app.post("/api/shorturl", bodyParser.urlencoded({ extended: false }) , function(req, res) {
+  let response = {};
+  inputUrl = req.body['url'];
+
+  response['original_url'] = inputUrl;
+
+  let inputShort = 1;
+
+  Url.findOne({})
+    .sort({ short_url: 'desc'})
+    .exec(function(error, result) {
+      if(!error && result != undefined){
+        inputShort = result.short + 1;
+      }
+      if(!error){
+        Url.findOneAndUpdate(
+          {original_url: inputUrl},
+          {original_url: inputUrl, short_url: inputShort},
+          {new: truw, upsert: true},
+          function(error, savedUrl) {
+            if(!error){
+              response['short_url'] = savedUrl.short;
+              response.json(response);
+            }
+          }
+        )
+      }
+    });
+
+  res.json(response);
 });
